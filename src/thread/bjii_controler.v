d56db294c257fcb625fc7743efef945f6d8ae51b
Harry Le Poer Trench <HarryLePoerTrench@gmail.com>
2026-07-08 01:26:04 +1000

    added thread components

---
+++ src/thread/bjii_controler.v
@@ -0,0 +1,9 @@
+// branch jump incriment interrupt controller TODO: get a better name for this nonsense
+// handles decisions on how to branch, jump or incriment
+
+
+module bjii_controler #() (
+
+);
+
+endmodule
---
+++ src/thread/decoder.v
@@ -0,0 +1,7 @@
+// decodes the instructions into a series of timed control signals
+
+module decoder #(parameter instruction_width = 32) (
+
+);
+
+endmodule
