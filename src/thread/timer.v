bc5e2f6906b6993d209045f474fb4d7117947b6d
Harry Le Poer Trench <HarryLePoerTrench@gmail.com>
2026-07-08 14:27:20 +1000

    timer now triggers overflow sig

--- src/thread/timer.v
+++ src/thread/timer.v
@@ -1,5 +1,6 @@
 // inbuilt alarm/clock
 // theres no way this wont be useful
+// triggers attempted program change as an interrupt
 // 32 bit for obv reasons
 
 module timer #(parameter timer_width = 32) (
@@ -7,7 +8,7 @@ module timer #(parameter timer_width = 32) (
     input wire timer_to_bus,
     input wire reset,
     output wire bus,
-    output wire timer_overflow_sig,
+    output wire timer_overflow_sig
 );
 
     reg [timer_width-1:0] timer;
@@ -27,5 +28,6 @@ module timer #(parameter timer_width = 32) (
         end
     end
 
-    assign bus = (timer_to_bus == 1'b1) ? timer : {timer_width{1'bz}};
+    assign timer_overflow_sig = (&timer); // iff all ones, is set, else is not
+    assign bus = (timer_to_bus) ? timer : {timer_width{1'bz}};
 endmodule
